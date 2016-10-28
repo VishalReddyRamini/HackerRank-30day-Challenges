3907e4805829155d3bf55f05c83285475d6ccc7a
VishalReddyRamini <vishalreddy.ramini@outlook.com>
2016-10-27 22:03:12 -0400

    Factorialrecursion

---
+++ hackerrank30/hackerrank30/RecursionFactorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerrank30
+{
+    class RecursionFactorial
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int n = int.Parse(Console.ReadLine());
+            int result = factorial(n);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public static int factorial(int k)
+        {
+            if (k > 1)
+            {
+                return k * factorial(k - 1);
+
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
